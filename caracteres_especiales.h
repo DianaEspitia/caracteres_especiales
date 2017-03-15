b3f9d052e667b80dc41d36a625b7e2b6ca5f67fd
Martínez Ortiz Saúl Axel <saulaxel.50@gmail.com>
2017-03-15 12:14:04 -0600

    Change name to be a .h file

--- caracteres_especiales.h
+++ caracteres_especiales.h
@@ -2,7 +2,7 @@
  * (C) Copyright 2017 MartInez Ortiz Saul UNAM
  *
  * This program is free software: you can redistribute it and/or modify
- * Tt under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
